2fd91db18575dc8db775843bf7fa2f02c9244cc8
ubuntu <ubuntu@ip-10-0-1-17>
2025-01-14 18:16:23 +0000

    waleko__claude-3-opus_9877__id_201

--- impacket/examples/ntlmrelayx/servers/socksserver.py
+++ impacket/examples/ntlmrelayx/servers/socksserver.py
@@ -460,7 +460,7 @@ class SocksRequestHandler(socketserver.BaseRequestHandler):
 
 
 class SOCKS(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    def __init__(self, server_address=('127.0.0.1', 1080), handler_class=SocksRequestHandler, api_port):
+    def __init__(self, server_address=('127.0.0.1', 1080), handler_class=SocksRequestHandler, api_port=None):
         LOG.info('SOCKS proxy started. Listening on %s:%d', server_address[0], server_address[1])
 
         self.activeRelays = {}
@@ -483,9 +483,10 @@ class SOCKS(socketserver.ThreadingMixIn, socketserver.TCPServer):
         self.__timer = RepeatedTimer(KEEP_ALIVE_TIMER, keepAliveTimer, self)
 
         # Let's start our RESTful API
-        self.restAPI = Thread(target=webService(server_address[0], api_port), args=(self, ))
-        self.restAPI.daemon = True
-        self.restAPI.start()
+        if api_port:
+            self.restAPI = Thread(target=webService(server_address[0], api_port), args=(self, ))  
+            self.restAPI.daemon = True
+            self.restAPI.start()
 
         # Let's start out worker for active connections
         self.activeConnectionsWatcher = Thread(target=activeConnectionsWatcher, args=(self, ))
